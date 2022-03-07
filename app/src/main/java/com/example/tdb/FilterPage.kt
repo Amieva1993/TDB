2507852192c3180bb279088959e6b0bc5144ff40
Amieva Enzo <enzo_amieva@ens.univ-artois.fr>
2022-03-07 18:34:10 +0100

    ajout filtre

---
+++ app/src/main/java/com/example/tdb/FilterPage.kt
@@ -0,0 +1,14 @@
+package com.example.tdb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FilterPage:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.filter)
+
+
+    }
+}
